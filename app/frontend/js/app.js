8d269eb94661b7859753da49564523ce4fe5052e
N. G. Scheurich <nick@scheurich.me>
2017-10-18 12:41:45 -0500

    Add Breakfast and Tachyons
    Some basic styles will make development more pleasant

---
+++ app/frontend/js/app.js
@@ -0,0 +1,14 @@
+// Turbolinks - To disable remove the next two lines.
+// https://github.com/turbolinks/turbolinks
+import Turbolinks from 'turbolinks';
+Turbolinks.start();
+
+// Require https://github.com/rails/jquery-ujs
+import 'jquery-ujs';
+
+const App = {
+  init() {
+  }
+}
+
+module.exports = App;
---
+++ brunch-config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  files: {
+    javascripts: {
+      joinTo: 'app.js'
+    },
+    stylesheets: {
+      joinTo: 'app.css'
+    },
+    templates: {
+      joinTo: 'app.js'
+    }
+  },
+
+  plugins: {
+    babel: {
+      presets: ['es2015']
+    },
+    sass: {
+      options: {
+        includePaths: ['node_modules/tachyons-sass']
+      }
+    }
+  },
+
+  paths: {
+    watched: [
+      'app/frontend',
+    ],
+
+    public: 'public/assets'
+  },
+
+  conventions: {
+     assets: /^(app\/frontend\/images)/
+  },
+
+  npm: {
+    globals: {
+      $: 'jquery',
+      jQuery: 'jquery',
+      breakfast: 'breakfast-rails'
+    }
+  }
+};
