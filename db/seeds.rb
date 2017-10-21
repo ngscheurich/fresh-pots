9cb102cb0fb08c51dcebbf309257cc4d8dd3d592
N. G. Scheurich <nick@scheurich.me>
2017-10-20 21:36:30 -0500

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+pots = Pot.create(
+  [
+    { name: "Break Room" },
+    { name: "Kitchen" },
+    { name: "Basement" }
+  ]
+)
+
+coffee_types = CoffeeType.create(
+  [
+    { name: "Dark Roast" },
+    { name: "Coffee & Chicory" }
+  ]
+)
+
+1.upto(20) do
+  Brew.create(pot: pots.sample,
+              coffee_type: coffee_types.sample)
+end
