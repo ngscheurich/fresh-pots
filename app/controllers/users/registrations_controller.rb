a6267d766b9f58c24391773452452f015cc91ba4
N. G. Scheurich <nick@scheurich.me>
2017-10-23 17:14:43 -0500

    Formatting

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,15 +1,27 @@
-class Users::RegistrationsController < Devise::RegistrationsController
-  before_action :email_authorization, only: [:create]
+module Users
+  class RegistrationsController < Devise::RegistrationsController
+    before_action :email_authorization, only: [:create]
 
-  private
+    private
+
     def email_authorization
-      if Rails.configuration.email_authorization["enabled"]
-        user_domain = params["user"]["email"].split("@").last
-        allowed_domains = Rails.configuration.email_authorization["domains"]
-        unless allowed_domains.include?(user_domain)
-          flash[:error] = "Sorry, your email address is not authorized for this app."
-          redirect_to new_user_registration_url
-        end
-      end
+      return unless email_opts["enabled"]
+
+      user_domain = params["user"]["email"].split("@").last
+      allowed_domains = email_opts["domains"]
+
+      deny_registration unless allowed_domains.include?(user_domain)
     end
+
+    def deny_registration
+      custom_message = email_opts["message"]
+      default_message = "Sorry, your email address is not authorized for this app."
+      flash[:error] = custom_message.nil? ? default_message : custom_message
+      redirect_to new_user_registration_url
+    end
+
+    def email_opts
+      Rails.configuration.email_authorization
+    end
+  end
 end
