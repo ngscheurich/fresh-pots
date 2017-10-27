8a972e889f5aee23e87f095c11412bfcd3d13e54
N. G. Scheurich <nick@scheurich.me>
2017-10-27 16:58:36 -0500

    Fade brews out as they age

--- app/frontend/js/components/Brew.js
+++ app/frontend/js/components/Brew.js
@@ -6,15 +6,44 @@ import "moment-timezone";
 class Brew extends React.Component {
   constructor(props) {
     super(props);
-    let brew = props.brew;
-    let timezone = moment.tz.guess();
+    const brew = props.brew;
+    const timezone = moment.tz.guess();
+    const started = moment.tz(brew.created_at, timezone);
+
     this.state = {
       pot: brew.pot.name,
       type: brew.coffee_type.name,
-      started: moment.tz(brew.created_at, timezone),
-      timeAgo: moment.tz(brew.created_at, timezone).fromNow(),
-      timezone
+      timezone: timezone,
+      started: started,
+      timeAgo: started,
+      opacity: 1
     };
+
+    this.state.timeAgo = this.calcTimeAgo();
+    this.state.opacity = this.calcOpacity();
+  }
+
+  calcTimeAgo() {
+    return moment(this.state.started).tz(this.state.timezone);
+  }
+
+  calcOpacity() {
+    const maxAge = 180;
+    const minOpacity = 0.35;
+    const minutesOld = this.state.started.diff(moment(), "minutes");
+    let opacity = (maxAge + minutesOld) / maxAge + 0.05;
+    opacity = opacity > 1 ? 1 : opacity;
+
+    return opacity < minOpacity ? minOpacity : opacity;
+  }
+
+  tick() {
+    this.setState(
+      Object.assign(...this.state, {
+        timeAgo: this.calcTimeAgo(),
+        opacity: this.calcOpacity()
+      })
+    );
   }
 
   componentDidMount() {
@@ -25,27 +54,20 @@ class Brew extends React.Component {
     clearInterval(this.timerID);
   }
 
-  tick() {
-    this.setState(
-      Object.assign(...this.state, {
-        timeAgo: moment(this.state.started)
-          .tz(this.state.timezone)
-          .fromNow()
-      })
-    );
-  }
-
   render() {
     return (
-      <div className="recent-brew df mb4 br2 box bg-white-10">
-        <div className="recent-brew__when pv2 ph4 br b--black-20 df bg-white-05">
-          <div className="f4 fw3 as-end">
+      <div
+        className="recent-brew df mb4 br2 box bg-white-10"
+        style={{ opacity: this.state.opacity }}
+      >
+        <div className="pv3 ph4 br b--black-20 df bg-white-05">
+          <div className="recent-brew__when f4 fw3 as-center">
             {moment(this.state.started).format("h:mm a")}
           </div>
         </div>
         <div className="recent-brew__info fx-1">
-          <div className="recent-brew__meter f4 fw3 br-pill pa2 ma4 bb b--black-50">
-            Brewed {this.state.timeAgo}
+          <div className="recent-brew__meter f4 fw3 pa4 bb b--black-50">
+            Brewed {this.state.timeAgo.fromNow()}
           </div>
 
           <div className="recent-brew__details df pv2 ph4 bt b--black-10">
