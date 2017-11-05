19a0f3f4abea251b9aa5b3321bd6de4f02cfbe31
N. G. Scheurich <nick@scheurich.me>
2017-11-05 13:38:49 -0600

    Add animate.css load path

--- brunch-config.js
+++ brunch-config.js
@@ -1,44 +1,44 @@
 module.exports = {
   files: {
     javascripts: {
-      joinTo: 'app.js'
+      joinTo: "app.js"
     },
     stylesheets: {
-      joinTo: 'app.css'
+      joinTo: "app.css"
     },
     templates: {
-      joinTo: 'app.js'
+      joinTo: "app.js"
     }
   },
 
   plugins: {
     babel: {
-      presets: ['es2015', 'react']
+      presets: ["es2015", "react"]
     },
     sass: {
       options: {
-        includePaths: ['node_modules/tachyons-sass']
+        includePaths: [
+          "node_modules/tachyons-sass",
+          "./node_modules/animate.css"
+        ]
       }
     }
   },
 
   paths: {
-    watched: [
-      'app/frontend',
-    ],
-
-    public: 'public/assets'
+    watched: ["app/frontend"],
+    public: "public/assets"
   },
 
   conventions: {
-     assets: /^(app\/frontend\/images)/
+    assets: /^(app\/frontend\/images)/
   },
 
   npm: {
     globals: {
-      $: 'jquery',
-      jQuery: 'jquery',
-      breakfast: 'breakfast-rails'
+      $: "jquery",
+      jQuery: "jquery",
+      breakfast: "breakfast-rails"
     }
   }
 };
