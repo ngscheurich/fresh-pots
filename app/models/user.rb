4e6665943d4bed04ccfb5f0845221e8d018bfa14
N. G. Scheurich <nick@scheurich.me>
2017-11-05 13:52:13 -0600

    Make user model aware of avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_attached_file :avatar, s3_region: ENV["aws_region"]
+
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
