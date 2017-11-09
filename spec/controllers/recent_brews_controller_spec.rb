df75ebafde382bfd951a61a1b5adad42d841a87e
N. G. Scheurich <nick@scheurich.me>
2017-11-09 17:25:25 -0600

    Test recent_brews

---
+++ spec/controllers/recent_brews_controller_spec.rb
@@ -0,0 +1,18 @@
+require "rails_helper"
+
+describe RecentBrewsController do
+  login_user
+
+  describe "GET #index" do
+    it "assigns @recent_brews" do
+      brew = create(:brew)
+      get :index, params: { format: :json }
+      expect(assigns(:recent_brews)).to eq([brew])
+    end
+
+    it "renders the index template" do
+      get :index, params: { format: :json }
+      expect(response).to render_template("index")
+    end
+  end
+end
