6630663e09b6f98638d9983be89b246f904f7306
N. G. Scheurich <nick@scheurich.me>
2017-11-09 17:26:15 -0600

    Add method to get most active user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,8 @@ class User < ApplicationRecord
   has_attached_file :avatar, s3_region: ENV["aws_region"]
 
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
+  def self.most_active
+    User.order(brews_count: :desc).first
+  end
 end
