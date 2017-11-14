9b240cdf2319ef570800b7a994e42b1e90878e81
N. G. Scheurich <nick@scheurich.me>
2017-11-14 17:18:53 -0600

    Set max brew hours

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module FreshPots
   class Application < Rails::Application
     config.action_controller.action_on_unpermitted_parameters = :raise
     config.fresh_pots = config_for(:fresh_pots)
+    config.fresh_pots["max_brew_hours"] ||= "5"
     config.load_defaults 5.1
     config.generators do |generate|
       generate.assets false
