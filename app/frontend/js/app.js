5dab0f9c34ab4ccdd3a81e639ee6623817b53fe4
N. G. Scheurich <nick@scheurich.me>
2017-12-15 11:20:46 -0600

    Render charts

--- app/frontend/js/app.js
+++ app/frontend/js/app.js
@@ -5,6 +5,8 @@ import CurrentTime from "./components/CurrentTime";
 import RecentBrews from "./components/RecentBrews";
 import Toast from "./components/Toast";
 import UserMenu from "./components/UserMenu";
+import BrewTimesChart from "./components/BrewTimesChart";
+import BrewDoughnutChart from "./components/BrewDoughnutChart";
 import "rails-ujs";
 
 Turbolinks.start();
@@ -27,12 +29,37 @@ function renderToast() {
   }
 }
 
+function renderBrewTimesChart() {
+  const node = document.querySelector("[data-component='brew-times-chart']");
+
+  if (node) {
+    const data = JSON.parse(node.dataset.chartData);
+    ReactDOM.render(<BrewTimesChart chartData={data} />, node);
+  }
+}
+
+function renderBrewDoughnutCharts() {
+  const nodes = document.querySelectorAll(
+    "[data-component='brew-doughnut-chart']"
+  );
+  console.log(nodes);
+
+  if (nodes.length > 0) {
+    nodes.forEach(node => {
+      const data = JSON.parse(node.dataset.chartData);
+      ReactDOM.render(<BrewDoughnutChart chartData={data} />, node);
+    });
+  }
+}
+
 const App = {
   init() {
     document.addEventListener("turbolinks:load", () => {
       renderComponent(<CurrentTime />, "#current-time");
       renderComponent(<RecentBrews />, "#brew-list");
       renderComponent(<UserMenu />, "#user-menu");
+      renderBrewTimesChart();
+      renderBrewDoughnutCharts();
       renderToast();
     });
   }
