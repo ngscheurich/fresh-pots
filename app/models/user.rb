d6f6957183be715551b290a19d13109db474dc42
N. G. Scheurich <nick@scheurich.me>
2017-12-20 14:50:09 -0600

    Set avatar fallback URL

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_attached_file :avatar, s3_region: ENV["aws_region"]
+  has_attached_file(
+    :avatar,
+    s3_region: ENV["aws_region"],
+    default_url: lambda do |a|
+      "https://api.adorable.io/avatars/200/#{a.instance.email}"
+    end
+  )
 
   validates_attachment_content_type :avatar, content_type: %r{\Aimage\/.*\z}
 
