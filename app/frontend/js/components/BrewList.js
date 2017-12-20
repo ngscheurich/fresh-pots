771d7c10c320ad79d7a88168eeb74361f1969843
N. G. Scheurich <nick@scheurich.me>
2017-12-20 14:54:07 -0600

    Formatting

--- app/frontend/js/components/BrewList.js
+++ app/frontend/js/components/BrewList.js
@@ -1,5 +1,5 @@
-import PropTypes from "prop-types";
 import React from "react";
+import PropTypes from "prop-types";
 import { ToastContainer, toast } from "react-toastify";
 import LoadingIndicator from "react-loading-indicator";
 import Brew from "./Brew";
