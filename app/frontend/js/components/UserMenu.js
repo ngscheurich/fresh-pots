5a5e1253e955813304fa6c31d89b4725df65dd57
N. G. Scheurich <nick@scheurich.me>
2017-12-20 14:54:56 -0600

    Add user menu dropdown

--- app/frontend/js/components/UserMenu.js
+++ app/frontend/js/components/UserMenu.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Radium from "radium";
+import PropTypes from "prop-types";
 
 class UserMenu extends Component {
   constructor(props) {
@@ -16,73 +16,31 @@ class UserMenu extends Component {
 
   render() {
     return (
-      <div>
+      <div className="fr relative">
         <button
-          className="pointer bn input-reset bg-transparent dim pa0"
+          className="pr4 input-reset bn bg-transparent pointer"
+          style={{
+            background:
+              "center right / 30% no-repeat url(/assets/icons/down.svg)"
+          }}
           onClick={this.handleToggleMenu}
         >
-          <img
-            className="v-mid o-80 burger"
-            src="/assets/icons/burger.svg"
-            alt="Menu icon"
-          />
+          <img className="avatar br-100" src={this.props.avatar} />
         </button>
-
         <div
-          className="fixed z-2 right-0 top-0 h-100 fw3 w-33-ns"
-          style={[
-            styles.base,
-            this.state.isOpen ? styles.expanded : styles.isClosed
-          ]}
+          className="absolute right-0 bg-red white pa4 dn"
+          style={{ top: 40, display: this.state.isOpen ? "block" : "none" }}
         >
-          <a
-            className="db ma4 link dim white-80 ttu f4"
-            href="/users/sign_out"
-            rel="nofollow"
-            data-method="delete"
-          >
-            <img
-              className="v-mid mr2 o-50"
-              src="/assets/icons/leave.svg"
-              alt=""
-              style={{ height: "36px" }}
-            />
-            Sign out
-          </a>
-
-          <a className="db ma4 link dim white-80 ttu f4" href="#">
-            <img
-              className="v-mid mr2 o-50"
-              src="/assets/icons/edit.svg"
-              alt="Sign out icon"
-              style={{ height: "36px" }}
-            />
-            Edit your profile
-          </a>
-
-          <div
-            className="fixed left-0 top-0"
-            style={{ width: "100vw", height: "100vh", zIndex: -1 }}
-            onClick={this.handleToggleMenu}
-          />
+          <a href={this.props.editUrl}>Edit profile</a>
         </div>
       </div>
     );
   }
 }
 
-const styles = {
-  base: {
-    backgroundColor: "#222222",
-    boxShadow: "0 0 30px rgba(0, 0, 0, 0.4)",
-    transition: "all 0.2s ease-in-out"
-  },
-  isOpen: {
-    transform: "translateX(0)"
-  },
-  isClosed: {
-    transform: "translateX(100%)"
-  }
+UserMenu.propTypes = {
+  avatar: PropTypes.string,
+  editUrl: PropTypes.string
 };
 
-export default Radium(UserMenu);
+export default UserMenu;
