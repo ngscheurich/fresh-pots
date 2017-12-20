1e81355824eb5e24251654186a0602e1be9924df
N. G. Scheurich <nick@scheurich.me>
2017-12-20 14:49:45 -0600

    Add first and last name to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171215172339) do
+ActiveRecord::Schema.define(version: 20171217173217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -62,6 +62,8 @@ ActiveRecord::Schema.define(version: 20171215172339) do
     t.integer "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.integer "brews_count", default: 0
+    t.string "first_name"
+    t.string "last_name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
