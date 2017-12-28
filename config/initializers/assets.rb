610cee5f2664170b66c30befb7cc1191dcc08dc3
N. G. Scheurich <nick@scheurich.me>
2017-12-28 17:51:04 -0600

    Configure assets

---
+++ config/initializers/assets.rb
@@ -0,0 +1,6 @@
+Rails.application.config.assets.version = "1.0"
+
+Rails.application.config.assets.paths << Rails.root.join("node_modules")
+Rails.application.config.assets.paths << Rails.root.join("public", "assets")
+
+Rails.application.config.assets.precompile += %w( app.css app.js )
