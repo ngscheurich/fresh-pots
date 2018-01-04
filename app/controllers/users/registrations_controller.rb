fa7dd1f79fa79c25ab6fb5c7f88d83be92534876
N. G. Scheurich <nick@scheurich.me>
2018-01-04 11:18:51 -0600

    Re-enable domain filtering

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,30 +1,30 @@
 module Users
   class RegistrationsController < Devise::RegistrationsController
-    # before_action :email_authorization, only: [:create]
+    before_action :email_authorization, only: [:create]
 
-    # private
+    private
 
-    # def email_authorization
-    #   return unless whitelist["enabled"]
+    def email_authorization
+      return unless whitelist["enabled"]
 
-    #   user_domain = params["user"]["email"].split("@").last
-    #   allowed_domains = whitelist["domains"]
+      user_domain = params["user"]["email"].split("@").last
+      allowed_domains = whitelist["domains"]
 
-    #   deny_registration unless allowed_domains.include?(user_domain)
-    # end
+      deny_registration unless allowed_domains.include?(user_domain)
+    end
 
-    # def deny_registration
-    #   custom_message = whitelist["message"]
-    #   flash[:error] = custom_message.nil? ? default_message : custom_message
-    #   redirect_to new_user_registration_url
-    # end
+    def deny_registration
+      custom_message = whitelist["message"]
+      flash[:error] = custom_message.nil? ? default_message : custom_message
+      redirect_to new_user_registration_url
+    end
 
-    # def whitelist
-    #   Rails.configuration.fresh_pots["email_whitelist"]
-    # end
+    def whitelist
+      Rails.configuration.fresh_pots["email_whitelist"]
+    end
 
-    # def default_message
-    #   "Sorry, your email address is not authorized for this app."
-    # end
+    def default_message
+      "Sorry, your email address is not authorized for this app."
+    end
   end
 end
