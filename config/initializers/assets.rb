7e4e506a9c1159f26e923fc1284fbed01abebfe1
N. G. Scheurich <nick@scheurich.me>
2018-01-04 23:25:31 -0600

    Re-enable asset pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,6 +1,6 @@
-# Rails.application.config.assets.version = "1.2"
+Rails.application.config.assets.version = "1.3"
 
-# Rails.application.config.assets.paths << Rails.root.join("node_modules")
-# Rails.application.config.assets.paths << Rails.root.join("public", "assets")
+Rails.application.config.assets.paths << Rails.root.join("node_modules")
+Rails.application.config.assets.paths << Rails.root.join("public", "assets")
 
-# Rails.application.config.assets.precompile += %w( app.css app.js )
+Rails.application.config.assets.precompile += %w( app.css app.js )
