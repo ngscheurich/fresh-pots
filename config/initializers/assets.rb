a72dba397119af2ee9dbe532fe43d9b3bb606367
N. G. Scheurich <nick@scheurich.me>
2018-01-04 22:50:09 -0600

    Bump assets version

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,4 +1,4 @@
-Rails.application.config.assets.version = "1.1"
+Rails.application.config.assets.version = "1.2"
 
 Rails.application.config.assets.paths << Rails.root.join("node_modules")
 Rails.application.config.assets.paths << Rails.root.join("public", "assets")
