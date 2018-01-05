ecff42cc2b8af81ad1f69be50cb4fe59d7f6643f
N. G. Scheurich <nick@scheurich.me>
2018-01-05 02:16:51 -0600

    Skip feature testing for now

--- spec/features/user_updates_their_profile_spec.rb
+++
@@ -1,5 +0,0 @@
-require "rails_helper"
-
-feature "User updates their profile" do
-  # TODO: Complete this test
-end
--- spec/features/user_visits_home_page_spec.rb
+++
@@ -1,9 +0,0 @@
-require "rails_helper"
-
-feature "User visits home page" do
-  scenario "sees page title" do
-    visit root_path
-
-    expect(page).to have_css "h1", text: "Fresh Pots"
-  end
-end
